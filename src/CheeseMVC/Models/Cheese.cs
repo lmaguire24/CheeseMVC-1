9fba47c78c4192a6bf84cc5e88772f87c89b0ee2
Lenon <lenon.maguire@gmail.com>
2017-04-10 14:08:41 -0700

    This is the proper save after lesson 3.12 code along video

--- src/CheeseMVC/Models/Cheese.cs
+++ src/CheeseMVC/Models/Cheese.cs
@@ -5,14 +5,7 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public CheeseType Type { get; set; }
-
-        public int CheeseId { get; set; }
-        private static int nextId = 1;
-
-        public Cheese()
-        {
-            CheeseId = nextId;
-            nextId++;
-        }
+        public int ID { get; set; }
+        
     }
 }
